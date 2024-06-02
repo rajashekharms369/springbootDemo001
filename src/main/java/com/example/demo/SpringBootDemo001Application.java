6dc720cf84f92df280385c1c5b44306d4fd9657f
rajashekharms369 <rajmkumar369@gmail.com>
2024-06-02 17:57:19 +0530

    Initial commit

--- src/main/java/com/example/demo/SpringBootDemo001Application.java
+++ src/main/java/com/example/demo/SpringBootDemo001Application.java
@@ -9,6 +9,7 @@ public class SpringBootDemo001Application {
 	public static void main(String[] args) {
 //		SpringApplication.run(SpringBootDemo001Application.class, args);
 		System.out.println("Hello world");
+		System.out.println("Good morning");
 	}
 
 }
