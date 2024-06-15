a585d481e9f7061ebb848ead38922c65d4c05a6d
rajashekharms369 <rajmkumar369@gmail.com>
2024-06-15 20:49:10 +0530

    Initial commit

--- src/main/java/com/example/demo/SpringBootDemo001Application.java
+++ src/main/java/com/example/demo/SpringBootDemo001Application.java
@@ -1,15 +1,10 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDemo001Application {
-
 	public static void main(String[] args) {
-//		SpringApplication.run(SpringBootDemo001Application.class, args);
-		System.out.println("Hello world");
-		System.out.println("Good morning");
+		SpringApplication.run(SpringBootDemo001Application.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/demo/entity/TreeNode.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+public class TreeNode {
+	private int data;
+	private TreeNode left;
+	private TreeNode right;
+	
+	public TreeNode(int data) {
+		this.data = data;
+	}
+}
